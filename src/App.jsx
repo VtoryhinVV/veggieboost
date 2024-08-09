788cc4eeef64cc70d2a7f8bb2ebaee77da8c64cc
Vlad Vtoryhin <vlad.vtorygin@gmail.com>
2024-08-09 10:58:52 +0300

    Upd modat css

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,96 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/HomePage";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-import HowItWorksPage from "./pages/HowItWorksPage";
-import VegetablesPage from "./pages/Vegetables";
-import ContactPage from "./pages/ContactPage";
-import ModalOrder from "./components/modal/modalOrder";
-import { useState } from "react";
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
+import ModalOrder from './components/modal/modalOrder'
+import ContactPage from './pages/ContactPage'
+import Home from './pages/HomePage'
+import HowItWorksPage from './pages/HowItWorksPage'
+import VegetablesPage from './pages/Vegetables'
 
 function App() {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [orderList, setOrderList] = useState([]);
+	const [modalIsOpen, setIsOpen] = useState(false)
+	const [orderList, setOrderList] = useState([])
 
-  function openModal() {
-    setIsOpen(true);
-  }
-  function closeModal() {
-    setIsOpen(false);
-  }
+	function openModal() {
+		setIsOpen(true)
+	}
+	function closeModal() {
+		setIsOpen(false)
+	}
 
-  function addProductToList(prod) {
-    if (orderList.find((option) => option.name === prod.name)) {
-      handleDeleteProd(prod.name);
-      return;
-    }
-    setOrderList([...orderList, prod]);
-  }
-  function handleDeleteProd(name) {
-    setOrderList(orderList.filter((option) => option.name !== name));
-  }
+	function addProductToList(prod) {
+		if (orderList.find(option => option.name === prod.name)) {
+			handleDeleteProd(prod.name)
+			return
+		}
+		setOrderList([...orderList, prod])
+	}
+	function handleDeleteProd(name) {
+		setOrderList(orderList.filter(option => option.name !== name))
+	}
 
-  function handleChangeWeight(name, weight) {
-    const updatedItems = orderList.map((item) =>
-      item.name === name ? { ...item, weightOrder: weight } : item
-    );
-    setOrderList(updatedItems);
-  }
-  function order(name) {
-    if (orderList.find((prod) => prod.name === name)) {
-      return true;
-    }
-    return false;
-  }
+	function handleChangeWeight(name, weight) {
+		const updatedItems = orderList.map(item =>
+			item.name === name ? { ...item, weightOrder: weight } : item
+		)
+		setOrderList(updatedItems)
+	}
+	function order(name) {
+		if (orderList.find(prod => prod.name === name)) {
+			return true
+		}
+		return false
+	}
 
-  return (
-    <>
-      <Header openModal={openModal} amount={orderList.length} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              addProd={addProductToList}
-              orderList={order}
-              openModal={openModal}
-            />
-          }
-        />
-        <Route path="/how_it_works" element={<HowItWorksPage />} />
-        <Route
-          path="/products"
-          element={
-            <VegetablesPage
-              addProd={addProductToList}
-              orderList={order}
-              openModal={openModal}
-            />
-          }
-        />
-        <Route path="/contacts" element={<ContactPage />} />
-        <Route
-          path="*"
-          element={
-            <Home
-              addProd={addProductToList}
-              orderList={order}
-              openModal={openModal}
-            />
-          }
-        />
-      </Routes>
-      <ModalOrder
-        close={closeModal}
-        status={modalIsOpen}
-        list={orderList}
-        handleChangeWeight={handleChangeWeight}
-        del={handleDeleteProd}
-      />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header openModal={openModal} amount={orderList.length} />
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<Home
+							addProd={addProductToList}
+							orderList={order}
+							openModal={openModal}
+						/>
+					}
+				/>
+				<Route path='/how_it_works' element={<HowItWorksPage />} />
+				<Route
+					path='/products'
+					element={
+						<VegetablesPage
+							addProd={addProductToList}
+							orderList={order}
+							openModal={openModal}
+						/>
+					}
+				/>
+				<Route path='/contacts' element={<ContactPage />} />
+				<Route
+					path='*'
+					element={
+						<Home
+							addProd={addProductToList}
+							orderList={order}
+							openModal={openModal}
+						/>
+					}
+				/>
+			</Routes>
+			<ModalOrder
+				close={closeModal}
+				status={modalIsOpen}
+				list={orderList}
+				handleChangeWeight={handleChangeWeight}
+				del={handleDeleteProd}
+			/>
+			<Footer />
+		</>
+	)
 }
 
-export default App;
+export default App
